8e11be112609b835bef98c8e9aa4e70fd258fb98
shreyash belote <shreyash.belote04@gmail.com>
2026-05-21 13:24:50 +0530

    testing CICD workflow

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js DevOps Project!');
+    res.send('CI/CD Pipeline Working Successfully!');
 });
 
 app.listen(PORT, () => {
